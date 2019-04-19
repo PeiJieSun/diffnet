80ec48a45cb1f49b872102e5a829a9531c058386
PeijieSun <PeiJieSun@users.noreply.github.com>
2019-04-19 15:26:33 -0500

    Update DataUtil.py

--- class/DataUtil.py
+++ class/DataUtil.py
@@ -19,9 +19,9 @@ class DataUtil():
     
     def createTrainHandle(self):
         data_dir = self.conf.data_dir
-        train_filename = "%s.train.json" % data_dir
-        val_filename = "%s.val.json" % data_dir
-        test_filename = "%s.test.json" % data_dir
+        train_filename = "%s.train.rating" % data_dir
+        val_filename = "%s.val.rating" % data_dir
+        test_filename = "%s.test.rating" % data_dir
 
         self.train = DataModule(self.conf, train_filename)
         self.val = DataModule(self.conf, val_filename)
