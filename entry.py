ebfd95b2cf88c765adae264bc2c34de7b8740503
PeijieSun <PeiJieSun@users.noreply.github.com>
2019-04-19 16:39:43 -0500

    Update entry.py

--- entry.py
+++ entry.py
@@ -25,33 +25,12 @@ def executeTrainModel(config_path, model_name):
 
     import train as starter
     starter.start(conf, data, model, evaluate)
-    
-def executeTestModel(config_path, model_name):
-    #print('System start to prepare parser config file...')
-    conf = ParserConf(config_path)
-    conf.parserConf()
-
-    #print('System start to load TensorFlow graph...')
-    model = eval(model_name)
-    model = model(conf)
-    if model.test_model:
-        model.test()
-
-    #print('System start to load data...')
-    data = DataUtil(conf, model)
-
-    evaluate = Evaluate(conf)
-
-    import mytest as starter
-    starter.start(conf, data, model, evaluate)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Welcome to the Experiment Platform Entry')
     parser.add_argument('--data_name', nargs='?', help='data name')
     parser.add_argument('--model_name', nargs='?', help='model name')
     parser.add_argument('--device_id', nargs='?', help='available gpu id')
-    parser.add_argument('--train', action='store_true', help='train flag')
-    parser.add_argument('--test', action='store_true', help='test flag')
 
     args = parser.parse_args()
 
@@ -62,7 +41,4 @@ if __name__ == "__main__":
     os.environ['CUDA_VISIBLE_DEVICES'] = device_id
     config_path = os.path.join(os.getcwd(), 'conf/%s_%s.ini' % (data_name, model_name))
 
-    if args.train:
-        executeTrainModel(config_path, model_name)
-    elif args.test:
-        executeTestModel(config_path, model_name)
+    executeTrainModel(config_path, model_name)
